95a1b1c5bde96007129ba3a8da4af8319a7c00fa
Audrey Carlsen <acarlsen@seattletimes.com>
2015-06-12 12:22:54 -0700

    what's a chart without some lines and labels?

--- src/js/main.js
+++ src/js/main.js
@@ -17,35 +17,27 @@ var percentiles = [
 ];
 
 var width = 620;
-var height = 350;
-var indexWidth = width / 6;
-var indexHeight = height / 7;
+var height = 310;
+var chartHeight = height - 30;
+var indexWidth = width / 7;
+var indexHeight = chartHeight / 7;
 
 var years = [2007, 2008, 2009, 2010, 2011, 2012, 2013];
 
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
 
-percentiles.forEach(function(percentile) {
-  var points = [];
-  years.forEach(function(year) {
-    var point = data[percentile.name + " percentile"][year].percent;
-    points.push(point);
-  });
+var render = function(index) {
+  ctx.font = "14px helvetica";
+  ctx.clearRect(0, 0, width, height); 
 
-  ctx.beginPath();
-  ctx.strokeStyle = percentile.color;
-  ctx.lineWidth = 1.5;
-  var i = 0;
-  points.forEach(function(point) {
-    var y = indexHeight - (point * 20);
-    ctx.lineTo(i, y);
-    ctx.stroke();
-    i += indexWidth;
-  });
-});
+  // hover bars
+  if (typeof index !== "undefined") {
+    ctx.fillStyle = "rgb(248, 248, 248)";
+    ctx.fillRect(index * indexWidth, 0, indexWidth, height);
+  }
 
-
+  // graph lines
   ctx.beginPath();
   ctx.strokeStyle = 'rgb(192, 192, 192)';
   ctx.lineWidth = 1;
@@ -56,6 +48,37 @@ percentiles.forEach(function(percentile) {
     ctx.lineTo(width, indexHeight*i);
   };
   ctx.stroke();
+
+  // year labels
+  var i = indexWidth/2;
+  ctx.fillStyle = "rgb(66, 70, 72)";
+  years.forEach(function(year){
+    ctx.fillText(year, i - 15, height);
+    i += indexWidth;
+  });
+
+  // data lines
+  percentiles.forEach(function(percentile) {
+    var points = [];
+    years.forEach(function(year) {
+      var point = data[percentile.name + " percentile"][year].percent;
+      points.push(point);
+    });
+
+    ctx.beginPath();
+    ctx.strokeStyle = percentile.color;
+    ctx.lineWidth = 1.75;
+    var i = indexWidth/2;
+    points.forEach(function(point) {
+      var y = indexHeight - (point * 20);
+      ctx.lineTo(i, y);
+      ctx.stroke();
+      i += indexWidth;
+    });
+  });
+};
+
+render();
 
 var tooltip = document.querySelector(".tooltip");
 
@@ -68,7 +91,8 @@ var onmove = function(e) {
     y: e.offsetY
   };
 
-  var index = (Math.round(position.x / indexWidth) + 2007).toString();
+  var index = Math.round(position.x / indexWidth);
+  var year = (index + 2007).toString();
   tooltip.classList.add("show");
   tooltip.style.top = e.pageY + 20 + "px";
   tooltip.style.left = e.pageX + 10 + "px";
@@ -78,12 +102,14 @@ var onmove = function(e) {
     if (percentile) {
       var name = percentile.name + " percentile";
       var color = percentile.color;
-      var income = data[percentile.name + " percentile"][index].income;
+      var income = data[percentile.name + " percentile"][year].income;
       values.push({
         name: name, color: color, income: income
       });
     }
   });
+
+  render(index);
 
   tooltip.innerHTML = ich.tooltipTemplate({data: values});
 };
