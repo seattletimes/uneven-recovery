1074805af5adefa6ceab967511d16aa3a95b7c26
Audrey Carlsen <acarlsen@seattletimes.com>
2015-06-17 12:34:28 -0700

    change data credit

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+//Use CommonJS style via browserify to load other modules
+// require("./lib/social");
+// require("./lib/ads");
+
+require("component-responsive-frame/child");
+require("./income.js");
+require("./jobs.js");
