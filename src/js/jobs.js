39220239e6f788e9369df992b1cd67c6f5ded669
Audrey Carlsen <acarlsen@seattletimes.com>
2015-06-17 17:10:00 -0700

    switch between state and county data

--- src/js/jobs.js
+++ src/js/jobs.js
@@ -5,7 +5,7 @@ var app = angular.module("job-recovery", []);
 var max = 0;
 var min = 0;
 
-jobsData.forEach(function(bracket) {
+jobsDataWA.forEach(function(bracket) {
   var width = bracket.total / 100;
   if (width < min) { min = width }
   if (width > max) { max = width }
@@ -14,7 +14,23 @@ jobsData.forEach(function(bracket) {
 min *= -1;
 var totalWidth = max + min;
 
-jobsData.forEach(function(bracket) {
+jobsDataWA.forEach(function(bracket) {
+  var width = bracket.total / 100;
+  if (width < 0) { 
+    bracket.firstWidth = (min + width) / min * 100;
+    bracket.secondWidth = (width * -1) / min * 100;
+    bracket.thirdWidth = 0;
+    bracket.fourthWidth = 100;
+  }
+  if (width > 0) { 
+    bracket.firstWidth = 100;
+    bracket.secondWidth = 0;
+    bracket.thirdWidth = width / max * 100;
+    bracket.fourthWidth = (max - width) / max * 100;
+  }
+});
+
+jobsDataKC.forEach(function(bracket) {
   var width = bracket.total / 100;
   if (width < 0) { 
     bracket.firstWidth = (min + width) / min * 100;
@@ -31,5 +47,11 @@ jobsData.forEach(function(bracket) {
 });
 
 app.controller("JobsController", ["$scope", function($scope) {
-  $scope.brackets = jobsData;
+  $scope.brackets = jobsDataWA;
+  $scope.selected = 'wa';
+  $scope.switchView = function(view) {
+    if (view == 'wa') { $scope.brackets = jobsDataWA }
+    if (view == 'kc') { $scope.brackets = jobsDataKC }
+    $scope.selected = view;
+  };
 }]);
