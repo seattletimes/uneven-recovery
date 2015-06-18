1c9b4fb991900b729ac3b75e8d4d55a4014ffec1
Audrey Carlsen <acarlsen@seattletimes.com>
2015-06-18 09:49:32 -0700

    i could watch these bars move for days

--- src/js/jobs.js
+++ src/js/jobs.js
@@ -14,37 +14,26 @@ jobsDataWA.forEach(function(bracket) {
 min *= -1;
 var totalWidth = max + min;
 
-jobsDataWA.forEach(function(bracket) {
-  var width = bracket.total / 100;
-  if (width < 0) { 
-    bracket.firstWidth = (min + width) / min * 100;
-    bracket.secondWidth = (width * -1) / min * 100;
-    bracket.thirdWidth = 0;
-    bracket.fourthWidth = 100;
-  }
-  if (width > 0) { 
-    bracket.firstWidth = 100;
-    bracket.secondWidth = 0;
-    bracket.thirdWidth = width / max * 100;
-    bracket.fourthWidth = (max - width) / max * 100;
-  }
-});
+var parseData = function(data) {
+  data.forEach(function(bracket) {
+    var width = bracket.total / 100;
+    if (width < 0) { 
+      bracket.firstWidth = (min + width) / min * 100;
+      bracket.secondWidth = (width * -1) / min * 100;
+      bracket.thirdWidth = 0;
+      bracket.fourthWidth = 100;
+    }
+    if (width > 0) { 
+      bracket.firstWidth = 100;
+      bracket.secondWidth = 0;
+      bracket.thirdWidth = width / max * 100;
+      bracket.fourthWidth = (max - width) / max * 100;
+    }
+  });
+};
 
-jobsDataKC.forEach(function(bracket) {
-  var width = bracket.total / 100;
-  if (width < 0) { 
-    bracket.firstWidth = (min + width) / min * 100;
-    bracket.secondWidth = (width * -1) / min * 100;
-    bracket.thirdWidth = 0;
-    bracket.fourthWidth = 100;
-  }
-  if (width > 0) { 
-    bracket.firstWidth = 100;
-    bracket.secondWidth = 0;
-    bracket.thirdWidth = width / max * 100;
-    bracket.fourthWidth = (max - width) / max * 100;
-  }
-});
+parseData(jobsDataWA);
+parseData(jobsDataKC);
 
 app.controller("JobsController", ["$scope", function($scope) {
   $scope.brackets = jobsDataWA;
